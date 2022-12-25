537eefc5fdf8a3694aac995c250ff25451f17cd0
Danim Kim <kdn725@gmail.com>
2022-12-25 22:36:12 +0900

    Refactor: remove unnessary style sheet

--- components/layouts/Navigation.tsx
+++ components/layouts/Navigation.tsx
@@ -1,19 +1,20 @@
-import * as S from './../header.style';
+import styled from '@emotion/styled';
 import Image from 'next/image';
 import Logo from './../../public/ala_logo.png';
+import { MEDIA_QUERY_END_POINT } from '../../constants';
 
 const Navigation = () => {
   return (
-    <S.Header>
-      <S.ImgContainer>
+    <Header>
+      <ImgContainer>
         <Image
           src={Logo}
           fill={true}
           sizes="(max-width: 768px) 90vw"
           alt="알라카르테"
         />
-      </S.ImgContainer>
-      <S.Nav>
+      </ImgContainer>
+      <Nav>
         <ul>
           <li>About</li>
           <li>유라</li>
@@ -23,9 +24,41 @@ const Navigation = () => {
           <li>장바구니</li>
           <li>나의계정</li>
         </ul>
-      </S.Nav>
-    </S.Header>
+      </Nav>
+    </Header>
   );
 };
 
 export default Navigation;
+
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0 2rem;
+  height: 60px;
+  @media screen and (max-width: ${MEDIA_QUERY_END_POINT.MOBILE}) {
+    height: 52px;
+  }
+`;
+
+const ImgContainer = styled.div`
+  position: relative; // Image 컴포넌트의 fill 속성 사용하기 위해 설정
+  min-width: 124px;
+  height: 28px;
+`;
+
+const Nav = styled.nav`
+  width: 100%;
+  margin-left: 2rem;
+  ul {
+    display: flex;
+    justify-content: space-between;
+    li {
+      font-weight: bold;
+    }
+  }
+  @media screen and (max-width: 768px) {
+    border: solid 1px violet;
+  }
+`;
