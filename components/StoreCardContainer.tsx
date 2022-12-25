9c11da4ddb9d9daee849b1fc5c66cfcb04683184
Danim Kim <kdn725@gmail.com>
2022-12-26 01:23:26 +0900

    Remove: delete file

--- components/StoreCardContainer.tsx
+++
@@ -1,7 +0,0 @@
-import { StoreInfoContainer } from './LocationPage.style';
-
-const StoreContainer = () => {
-  return <StoreInfoContainer></StoreInfoContainer>;
-};
-
-export default StoreContainer;
