b82e812497aab651449b9a9ab53cb81561276e40
Danim Kim <kdn725@gmail.com>
2022-12-26 01:23:08 +0900

    Design: style GNB menu

--- components/layouts/Navigation.tsx
+++ components/layouts/Navigation.tsx
@@ -2,6 +2,9 @@ import styled from '@emotion/styled';
 import Image from 'next/image';
 import Logo from './../../public/ala_logo.png';
 import { MEDIA_QUERY_END_POINT } from '../../constants';
+import Cart from './../../public/icon/cart.svg';
+import Account from './../../public/icon/account.svg';
+import { NAV_TAB_NAME } from '../../constants/Gnb';
 
 const Navigation = () => {
   return (
@@ -15,14 +18,25 @@ const Navigation = () => {
         />
       </ImgContainer>
       <Nav>
-        <ul>
-          <li>About</li>
-          <li>유라</li>
-          <li>알라카르테</li>
-          <li>공지사항</li>
-          <li>멤버십</li>
-          <li>장바구니</li>
-          <li>나의계정</li>
+        <ul className="container_menu">
+          <li>{NAV_TAB_NAME.TAB_1}</li>
+          <li>{NAV_TAB_NAME.TAB_2}</li>
+          <li>{NAV_TAB_NAME.TAB_3}</li>
+          <li>{NAV_TAB_NAME.TAB_4}</li>
+          <li>{NAV_TAB_NAME.TAB_5}</li>
+        </ul>
+        <ul className="container_icon-menu">
+          <li>
+            <Image src={Cart} alt={NAV_TAB_NAME.TAB_6} width={28} height={28} />
+          </li>
+          <li>
+            <Image
+              src={Account}
+              alt={NAV_TAB_NAME.TAB_7}
+              width={25}
+              height={25}
+            />
+          </li>
         </ul>
       </Nav>
     </Header>
@@ -35,8 +49,8 @@ const Header = styled.header`
   display: flex;
   align-items: center;
   justify-content: center;
-  padding: 0 2rem;
-  height: 60px;
+  padding: 0 2.5rem;
+  height: 100px;
   @media screen and (max-width: ${MEDIA_QUERY_END_POINT.MOBILE}) {
     height: 52px;
   }
@@ -51,14 +65,29 @@ const ImgContainer = styled.div`
 const Nav = styled.nav`
   width: 100%;
   margin-left: 2rem;
+  display: flex;
+  justify-content: space-between;
   ul {
-    display: flex;
-    justify-content: space-between;
+    display: grid;
     li {
       font-weight: bold;
+      font-size: 1.1rem;
     }
   }
-  @media screen and (max-width: 768px) {
+  ul.container_menu {
+    width: 90%;
+    grid-template-columns: repeat(5, 1fr);
+    text-align: center;
+  }
+  ul.container_icon-menu {
+    width: 10%;
+    min-width: 100px;
+    grid-template-columns: repeat(2, 1fr);
+    li {
+      text-align: right;
+    }
+  }
+  @media screen and (max-width: ${MEDIA_QUERY_END_POINT.MOBILE}) {
     border: solid 1px violet;
   }
 `;
